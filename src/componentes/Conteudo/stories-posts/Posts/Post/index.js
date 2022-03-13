aa516c805c7fa9332f24b3e3bdf84f59750092e4
samukaell <snoooydelta37@gmail.com>
2022-03-13 17:52:20 -0300

    Inserçao de post video

--- src/componentes/Conteudo/stories-posts/Posts/Post/index.js
+++ src/componentes/Conteudo/stories-posts/Posts/Post/index.js
@@ -8,7 +8,20 @@ export default function Post(props){
                 image = {props.image} 
                 name = {props.name} 
             />
-            <img src={props.imagePost} alt="gato fofo com telefone" class="image"/>
+            {props.type === 'typeImage' ? 
+                <img src={props.imagePost} alt="gato fofo com telefone" class="image"/> 
+
+                :
+                
+                <div className="conteudo-post">
+                    <video className="video" width="100%" height="80%" controls>
+                        <source src={props.videoPlay} type="video/mp4"/>
+                        <source src={props.videoController} type="video/ogg"/>
+                        Your browser does not support the video tag.
+                    </video>    
+                </div>
+
+            }
             <Feedback
                 imageCurtida = {props.imageCurtida}
                 curtidoPor = {props.curtidoPor}
--- src/componentes/Conteudo/stories-posts/Posts/index.js
+++ src/componentes/Conteudo/stories-posts/Posts/index.js
@@ -7,6 +7,8 @@ import './styles.css'
 //imagens
 import imagem1 from '../../../../midia/gato-telefone 1.png'
 import imagem2 from '../../../../midia/dog 1.png'
+import video from '../../../../midia/videos/video.mp4'
+import videoControle from '../../../../midia/videos/video.ogv'
 
 export default function Posts(){
     return (
@@ -14,6 +16,7 @@ export default function Posts(){
             <Post
                 image = {dataUser[1].image}
                 name = {dataUser[1].nome}
+                type= {"typeImage"}
                 imagePost = {imagem1}
                 imageCurtida = {dataUser[5].image}
                 curtidoPor = {dataUser[5].nome}
@@ -22,7 +25,18 @@ export default function Posts(){
             <Post
                 image = {dataUser[2].image}
                 name = {dataUser[2].nome}
+                type = {"typeImage"}
                 imagePost = {imagem2}
+                imageCurtida = {dataUser[8].image}
+                curtidoPor = {dataUser[8].nome}
+                porMais = {99.159}
+            />
+            <Post
+                image = {dataUser[2].image}
+                name = {dataUser[2].nome}
+                type = {"typeVideo"}
+                videoPlay = {video}
+                videoController = {videoControle}
                 imageCurtida = {dataUser[8].image}
                 curtidoPor = {dataUser[8].nome}
                 porMais = {99.159}
